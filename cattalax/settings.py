9075f6f0daff1a8ededfa2bf3a132c112633cec8
Tony O'Connor <toconnor13@gmail.com>
2013-12-01 12:21:22 +0000

    Automating site_url text input

--- cattalax/settings.py
+++ cattalax/settings.py
@@ -58,6 +58,8 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
+SITE_URL = os.environ['ALLAMEW_SITE_ID']
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
