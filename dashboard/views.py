8b82c8acf40c805f6e9c494a8a570da2e1173ff4
Tony O'Connor <toconnor13@gmail.com>
2013-12-13 17:29:20 +0000

    code cleaned

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,43 +8,47 @@ import datetime
 import time
 import random
 
+def get_percentage_graph(x_axis_data, time_periods_to_graph):
+	tooltip_date = "%A %d %b"
+	ydata = [float(v.get_capture_rate()) for v in time_periods_to_graph]
+	ydata2 = [float(v.get_bounce_rate()) for v in time_periods_to_graph]
+
+	extra_serie = {
+			"tooltip": {"y_start": "", "y_end": "%"},
+			"date_format": tooltip_date
+	}
+
+	chartdata = {'x': x_axis_data,
+			'name1': 'Capture Rate', 'y1': ydata, 'extra1': extra_serie,
+			'name2': 'Bounce Rate', 'y2': ydata2, 'extra2': extra_serie,
+	}
+	return chartdata
+
+def get_levels_graph(x_axis_data, time_periods_to_graph):
+	tooltip_date = "%A %d %b"
+	ydata = [float(v.no_of_entries) for v in time_periods_to_graph]
+	ydata2 = [float(v.no_of_bounces) for v in time_periods_to_graph]
+	ydata3 = [float(v.no_of_walkbys) for v in time_periods_to_graph]
+
+	extra_serie = {
+			"tooltip": {"y_start": "Visits: ", "y_end": ""},
+		"date_format": tooltip_date
+	}
+	chartdata = {'x': x_axis_data,
+		'name1': 'No. of Walkbys', 'y1': ydata3, 'extra1': extra_serie,
+		'name3': 'No. of Captures', 'y3': ydata, 'extra3': extra_serie,
+		'name2': 'No. of Bounces', 'y2': ydata2, 'extra2': extra_serie,
+	}
+	return chartdata
+
 def dashboard(request, levels=False):
 	days_to_graph = sorted(Day.objects.all(), key=Day.day_no)
 	xdata = map(lambda x: int(time.mktime(datetime.datetime(x.year, x.month, x.day, 12).timetuple())*1000), days_to_graph)
-	
-	
-	tooltip_date = "%A %d %b" 
 
 	if levels==False:
-		ydata = [float(v.get_capture_rate()) for v in days_to_graph]
-		ydata2 = [float(v.get_bounce_rate()) for v in days_to_graph]
-
-		extra_serie = {
-			"tooltip": {"y_start": "", "y_end": "%"},
-			"date_format": tooltip_date
-		}
-
-		chartdata1 = {'x': xdata, 
-			'name1': 'Capture Rate', 'y1': ydata, 'extra1': extra_serie,
-			'name2': 'Bounce Rate', 'y2': ydata2, 'extra2': extra_serie,
-		}
+		chartdata1 = get_percentage_graph(xdata, days_to_graph)
 	else:
-		ydata = [float(v.no_of_entries) for v in days_to_graph]
-		ydata2 = [float(v.no_of_bounces) for v in days_to_graph]
-		ydata3 = [float(v.no_of_walkbys) for v in days_to_graph]
-
-		extra_serie = {
-		#	"tooltip": {"y_start": "", "y_end": ""},
-			"date_format": tooltip_date
-		}
-
-
-		chartdata1 = {'x': xdata, 
-			'name1': 'No. of Walkbys', 'y1': ydata3, 'extra1': extra_serie,
-			'name3': 'No. of Captures', 'y3': ydata, 'extra3': extra_serie,
-			'name2': 'No. of Bounces', 'y2': ydata2, 'extra2': extra_serie,
-		}
-
+		chartdata1 = get_levels_graph(xdata, days_to_graph)
 
 	charttype1 = "lineChart"
 	chartcontainer = 'linechart_container' # container name
@@ -68,10 +72,8 @@ def dashboard(request, levels=False):
 
 def detail(request, day_id, levels=False):
 	d = get_object_or_404(Day, pk=day_id)
-
 	hours_to_show = [h for h in d.hour_set.all() if h.hour>6 and h.hour<20]
 	xdata = map(lambda h: str(h.hour), hours_to_show)
-
 	ydata = [v.avg_duration for v in hours_to_show]
 	
 	tooltip_date = "%A %d %b"
@@ -80,32 +82,11 @@ def detail(request, day_id, levels=False):
 		"date_format": tooltip_date,
 	}
 
-	extra_serie1 = {
-		"tooltip": {"y_start": "", "y_end": "%"},
-		"date_format": tooltip_date,
-	}
-
 	if levels==False:
-		ydata2 = [float(v.get_capture_rate()) for v in hours_to_show]
-		ydata3 = [float(v.get_bounce_rate()) for v in hours_to_show]
-
-		chartdata1 = {'x': xdata, 
-			'name1': 'Capture Rate', 'y1': ydata2, 'extra1': extra_serie1,
-			'name2': 'Bounce Rate', 'y2': ydata3, 'extra2': extra_serie1,
-		}
+		chartdata1 = get_percentage_graph(xdata, hours_to_show)
 
 	if levels==True:
-		ydata2 = [float(v.no_of_entries) for v in hours_to_show]
-		ydata3 = [float(v.no_of_walkbys) for v in hours_to_show]
-		ydata4 = [float(v.no_of_bounces) for v in hours_to_show]
-
-		chartdata1 = {'x': xdata, 
-			'name1': 'No. of Walkbys', 'y1': ydata3,# 'extra1': extra_serie2
-			'name3': 'No. of Captures', 'y3': ydata2,# 'extra3': extra_serie,
-			'name2': 'No. of Bounces', 'y2': ydata4,# 'extra2': extra_serie,
-		}
-
-	
+		chartdata1 = get_levels_graph(xdata, hours_to_show)
 	
 	chartdata2 = {'x': xdata,
 			'name1': 'Duration', 'y1': ydata, 'extra1': extra_serie2,
