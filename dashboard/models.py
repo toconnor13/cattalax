2c924a0864a2b2bed40fad3a3caf86e683d0b1d9
Tony O'Connor <toconnor13@gmail.com>
2014-02-04 17:21:28 +0000

    Base class added for times - undermodels edited

--- dashboard/models.py
+++ dashboard/models.py
@@ -24,6 +24,11 @@ class Visit(models.Model):
 	vendor = models.ForeignKey(Outlet)
 	duration = models.IntegerField()
 	arrival_time = models.IntegerField()
+	# A field is need here to attach the visit to an hour or day.  Preferably hour, but day will need to count the visits over all its children.
+	# arrival_time datetime field
+	# day foreign key field
+	# week foreign key field
+	# month foreign key field
 
 	def get_hour(self):
 		t = datetime.fromtimestamp(self.arrival_time)
@@ -32,72 +37,35 @@ class Visit(models.Model):
 class Walkby(models.Model):
 	vendor = models.ForeignKey(Outlet)
 	time = models.IntegerField()
+	# time datetime field
+	# day foreign key field
+	# week foreign key field
+	# month foreign key field
 	
 	def get_hour(self):
 		t = datetime.fromtimestamp(self.time)
 		return t.hour
 
-class Day(models.Model):
-	vendor = models.ForeignKey(Outlet)
-	day = models.IntegerField()
-	month = models.IntegerField()
-	year = models.IntegerField()
-	no_of_walkbys = models.IntegerField()
-	no_of_entries = models.IntegerField()
-	no_of_bounces = models.IntegerField()
-	avg_duration = models.IntegerField()
-
-	def day_no(self):
-		return int(self.day)
-
-	def describe(self):
-		d = datetime(self.year, self.month, self.day)
-		return d.strftime("%a %d %b")
-
-	def print_day(self):
-		d = datetime(self.year, self.month, self.day)
-		return d.strftime("%a %d")
-
-	def print_month(self):
-		d = datetime(self.year, self.month, self.day)
-		return d.strftime("%B %Y")
+# class Month:
 	
-	def get_capture_rate(self):
-		rate =  float(self.no_of_entries)*100/float(self.no_of_walkbys)
-		return str(round(rate,1)) 
 
-	def get_bounce_rate(self):
-		rate = float(self.no_of_bounces)*100/float(self.no_of_entries)
-		return str(round(rate,1))
-
-	def get_avg_duration(self):
-		mins = self.avg_duration / 60
-		secs = self.avg_duration % 60
-		if secs<10:
-			seconds = '0' + str(secs)
-		else:
-			seconds = str(secs)
-		return str(mins) + ':' + seconds
-
-class Hour(models.Model):
-	hour = models.IntegerField()
-	day = models.ForeignKey(Day)
+#class Week (models.Model):
+class TimeUnit(models.Model):
 	vendor = models.ForeignKey(Outlet)
 	no_of_walkbys = models.IntegerField()
 	no_of_entries = models.IntegerField()
 	no_of_bounces = models.IntegerField()
 	avg_duration = models.IntegerField()
-
-	def describe(self):
-		d = datetime(self.day.year, self.day.month, self.day.day, self.hour)
-		return d.strftime("%H:%M") # prepend %A for day string
+	# some datetime object?	
+	class Meta:
+		abstract = True
 
 	def get_capture_rate(self):
 		if (self.no_of_walkbys==0):
 			return '0'
 		else:
 			rate =  float(self.no_of_entries)*100/float(self.no_of_walkbys)
-			return str(round(rate,1)) 
+			return str(round(rate,1))
 
 	def get_bounce_rate(self):
 		if self.no_of_entries==0:
@@ -114,5 +82,33 @@ class Hour(models.Model):
 		else:
 			seconds = str(secs)
 		return str(mins) + ':' + seconds
+	
+class Day(TimeUnit):
+	day = models.IntegerField()
+	month = models.IntegerField()
+	year = models.IntegerField()
+	# a datetime object could combine the above three
 
+	def day_no(self):
+		return int(self.day)
+
+	def describe(self):
+		d = datetime(self.year, self.month, self.day)
+		return d.strftime("%a %d %b")
+
+	def print_day(self):
+		d = datetime(self.year, self.month, self.day)
+		return d.strftime("%a %d")
+
+	def print_month(self):
+		d = datetime(self.year, self.month, self.day)
+		return d.strftime("%B %Y")
+	
+class Hour(TimeUnit):
+	hour = models.IntegerField()
+	day = models.ForeignKey(Day)
+
+	def describe(self):
+		d = datetime(self.day.year, self.day.month, self.day.day, self.hour)
+		return d.strftime("%H:%M") # prepend %A for day string
 
