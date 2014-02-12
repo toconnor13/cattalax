89f89ed675208d26348f59cf9a7c37dba9f32000
Tony O'Connor <toconnor13@gmail.com>
2014-02-12 14:58:24 +0000

    Campaign from page edited to rm vert_nav | Left over events page eliminated from urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
 		url(r'^$', 'dashboard.views.dashboard', name="dashboard_home"),
 #		url(r'^contact/', 'dashboard.views.contact', name="contact_page"),
-		url(r'^event/', 'dashboard.views.event'),
+#		url(r'^event/', 'dashboard.views.event'),
 		url(r'^(?P<time_unit>\w+)/(?P<object_id>\d+)/$', 'dashboard.views.detail'),
 		url(r'^campaigns/', 'dashboard.views.campaigns'),
 		url(r'^add/', 'dashboard.views.campaign_form'),
