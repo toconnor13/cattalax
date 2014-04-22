3a9fcdc72a590ffa77e170a2d2efafaa18b7ea63
Tony O'Connor <toconnor13@gmail.com>
2014-04-22 17:22:16 +0100

    32/64 Bug fixed

--- auth/urls.py
+++ auth/urls.py
@@ -4,6 +4,11 @@
 # elsewhere.
 
 from django.conf.urls import patterns, url
+#from django.conf.urls.defaults import *
+
+#from django.contrib.auth import urls as auth_urls
+
+#urlpatterns = auth_urls.urlpatterns
 
 urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
@@ -12,8 +17,9 @@ urlpatterns = patterns('',
     url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
     url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
     url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
-    url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         'django.contrib.auth.views.password_reset_confirm',
         name='password_reset_confirm'),
     url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
 )
+
