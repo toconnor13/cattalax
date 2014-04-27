b49463e2889404d71740cd9e6992b5f63048b250
Tony O'Connor <toconnor13@gmail.com>
2014-04-27 12:45:25 +0100

    Dashboard dropdown button added

--- cattalax/views.py
+++ cattalax/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext, Context
 from django.core.mail.message import EmailMessage
+from dashboard.models import Outlet
 
 def index(request):
+	outlet_list = []
+	if request.user.is_authenticated:
+		outlet_list=Outlet.objects.filter(agent=request.user.username)
 	path = "/home/"
-	return render_to_response('index.html', {'path': path,}, context_instance=RequestContext(request))
+	return render_to_response('index.html', {'path': path, 'outlet_list': outlet_list,}, context_instance=RequestContext(request))
 
 def contact(request):
 	if request.method=='POST':
@@ -15,7 +19,7 @@ def contact(request):
 		details = name + ", " + email + ", " + company + ", " + number
 		msg=EmailMessage('Web contact Form', details, 'plazasight@gmail.com', ['plazasight@gmail.com'])
 		msg.send()
-		message = "Your details have been submitted.  A response should be issued withing 24 hours."
+		message = "Your details have been submitted.  A response should be issued within 24 hours."
 	else:
 		message = "No contact details have been submitted. Please resubmit the form."
 	return render_to_response('index.html', {'message': message,}, context_instance=RequestContext(request))
